e3904c1b83e9bf0c6fa120b546ad0cb9293e5fd2
john.data.chen <john.data.chen@gmail.com>
2015-05-30 22:06:20 +0800

    the crawler of PTT Tech_Job and Soft_Job
    the 1st spider can get the links of posts, then press previous page to
    get more links.

---
+++ ptt_tech_job/settings.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for ptt_tech_job project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'ptt_tech_job'
+
+SPIDER_MODULES = ['ptt_tech_job.spiders']
+NEWSPIDER_MODULE = 'ptt_tech_job.spiders'
+
+# how deep spider will crawl
+DEPTH_LIMIT=5
+
+# We don't want to be banned, so spider need to delay few seconds
+# 3 means 3 seconds
+DOWNLOAD_DELAY=3
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'ptt_tech_job (+http://www.yourdomain.com)'
---
+++ ptt_tech_job/spiders/post_link.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+from scrapy.http import Request
+
+from ptt_tech_job.items import PttTechJobItem
+
+
+class PostLinkSpider(scrapy.Spider):
+    name = "post_link"
+    allowed_domains = ["www.ptt.cc"]
+    start_urls = (
+        'https://www.ptt.cc/bbs/Tech_Job/index.html',
+		'https://www.ptt.cc/bbs/Soft_Job/index.html'
+    )
+
+    def parse(self, response):
+        item = PttTechJobItem()
+        # extract the links of posts in each page
+        item['url'] = response.css('div[class=title] a::attr(href)').extract()
+        yield item
+
+        # get the link of previous page
+        previous_page=response.xpath("//div[@class='btn-group pull-right']/a/@href").extract()[1]
+        # combine the domain and link
+        previous_page="https://www.ptt.cc" + previous_page
+        yield Request(previous_page, callback=self.parse)
